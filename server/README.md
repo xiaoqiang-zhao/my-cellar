37f27beaf227b2dd0ce24e234eed5fc6e87c7e0c
zhaoxiaoqiang <longze_xq@163.com>
2016-05-20 17:13:08 +0800

    添加对默认路径的支持

--- server/README.md
+++ server/README.md
@@ -3,7 +3,7 @@
 ## 启动命令
 
     node server
-    // 默认端口9999
+    // 网站默认端口9999，可在 server/lib/config.js 中配置
     
 ## 配置文件说明
     
--- service/rout-config.js
+++ service/rout-config.js
@@ -4,18 +4,18 @@
  * Created by v_zhaoxiaoqiang on 2015/6/3.
  */
 module.exports = [
+    //{
+    //    requestMethodType: 'GET',
+    //    contentType: 'html',
+    //    urlPath: '/articles/{articleId}', // 匹配的请求路径（不包括url参数）
+    //    modelPath: '/articles',           // 响应模块路径（其实是与server下config的servicePath做拼接）
+    //    methodName: 'data'                // 模块具体响应方法，默认是data方法
+    //},
     {
         requestMethodType: 'GET',
         contentType: 'html',
-        urlPath: '/articles/{articleId}', // 匹配的请求路径（不包括url参数）
-        modelPath: '/articles',           // 响应模块路径（其实是与server下config的servicePath做拼接）
-        methodName: 'data'                // 模块具体响应方法，默认是data方法
-    },
-    {
-        requestMethodType: 'POST',
-        contentType: 'html',
-        urlPath: '/articles/upload-file',
-        modelPath: '/articles/lib/upload-file',
-        methodName: 'data'
+        urlPath: '/',               // 匹配的请求路径（不包括url参数）
+        type: 'staticFile',         // 设置类型为静态文件重定向或别名
+        filePatch: '/index.html'    // 转到的文件
     }
 ];
