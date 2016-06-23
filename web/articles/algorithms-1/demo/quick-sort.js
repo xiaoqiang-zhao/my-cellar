878ccd4943ed694c4867401bfd22dc4a8e745de4
zhaoxiaoqiang <longze_xq@163.com>
2016-06-23 10:56:52 +0800

    完善文章 “算法-1”

--- web/articles/algorithms-1/demo/quick-sort.js
+++ web/articles/algorithms-1/demo/quick-sort.js
@@ -9,8 +9,10 @@ var data = [5, 2, 4, 6, 1, 3];
 function quickSort(array, from, to) {
     // 快速排序核心代码
     if (from < to) {
+        // 查找分水岭的位置
+        // 挖坑和填坑思路
         var spaceIndex;
-        var x = array[to]; // 主元
+        var x = array[to];
         var index = from - 1;
         for (var i = from; i < to - 1; i++) {
             if (array[i] <= x) {
@@ -20,8 +22,8 @@ function quickSort(array, from, to) {
                 array[i] = temp;
             }
         }
-
         spaceIndex = index + 1;
+
         arguments.callee(array, from, spaceIndex - 1);
         arguments.callee(array, spaceIndex + 1, to);
     }
