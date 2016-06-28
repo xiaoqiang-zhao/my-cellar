4a6ca42fa0544529b8f9f3e66bfffda187156fbc
zhaoxiaoqiang <longze_xq@163.com>
2016-06-28 18:39:16 +0800

    完成快排算法

--- web/articles/algorithms-1/demo/quick-sort.js
+++ web/articles/algorithms-1/demo/quick-sort.js
@@ -4,30 +4,41 @@
  * Created by zhaoxiaoqiang on 16/6/21.
  */
 
-var data = [5, 2, 4, 6, 1, 3];
-
+/**
+ * 对数组快速排序
+ *
+ * @param {Array} array 待排序的数组
+ * @param {number} from [可选]起始位置，在递归的时候才会用到此参数
+ * @param {number} to [可选]终止位置，，在递归的时候才会用到此参数
+ * @returns {Array} array 排序后的数组(是传入数组的址引用)
+ */
 function quickSort(array, from, to) {
+    // 参数容错，可使初始化不用做特殊处理
+    from = from || 0;
+    to = to === undefined ? (array.length - 1) : to;
+    // to || (array.length - 1); 此种写法对 to=0 的情况会出问题
+
     // 快速排序核心代码
-    if (from < to) {
-        // 查找分水岭的位置
-        // 挖坑和填坑思路
-        var spaceIndex;
-        var x = array[to];
-        var index = from - 1;
-        for (var i = from; i < to - 1; i++) {
-            if (array[i] <= x) {
-                index++;
-                var temp = array[index];
-                array[index] = array[i];
+    if (from < to) { // 实际上结束低递归的条件是 from === to
+
+        var markIndex = from - 1;  // 标记位
+        var spaceItem = array[to]; // 默认最后一个
+
+        for (var i = from; i <= to; i++) {
+            if (array[i] <= spaceItem) {
+                markIndex++;
+                var temp = array[markIndex];
+                array[markIndex] = array[i];
                 array[i] = temp;
             }
         }
-        spaceIndex = index + 1;
 
-        arguments.callee(array, from, spaceIndex - 1);
-        arguments.callee(array, spaceIndex + 1, to);
+        arguments.callee(array, from, markIndex - 1);
+        arguments.callee(array, markIndex + 1, to);
     }
     return array;
 }
 
-console.log(quickSort(data, 0, data.length - 1));
+console.log(quickSort([5, 2, 4, 6, 1, 3]));
+console.log(quickSort([6, 1, 3, 5, 2, 4]));
+console.log(quickSort([5, 2, 4, 6, 1, 1]));
