751224d53cbb0c63fc3d15206049cca5bc419771
longze <longze_xq@163.com>
2017-12-09 22:55:55 +0800

    添加内容：mongoose CRUD Demo

--- web/articles/mongo-db/demo/index.js
+++ web/articles/mongo-db/demo/index.js
@@ -2,8 +2,9 @@
  * Demo
  */
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
+const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://127.0.0.1/mydb', {
     useMongoClient: true
@@ -13,15 +14,74 @@ mongoose.connect('mongodb://127.0.0.1/mydb', {
     console.log('数据库链接失败:', err);
 });
 
-var User = mongoose.model('User', {
+// 断开数据库链接
+setTimeout(() => {
+    mongoose.disconnect(function(){
+        console.log("断开连接");
+    })
+}, 2000);
+
+// 两种写法之一：中规中矩
+const userSchema = new Schema({
     name: String
 });
 
+const User = mongoose.model('User', userSchema);
+
+// 两种写法之二：简化
+// const User = mongoose.model('User', {
+//     name: String
+// });
+
+
 var user = new User({
+    name: 'bbb'
+});
+
+// Create
+// user.save().then(() => {
+//     console.log('user 保存成功');
+// }, res => {
+//     console.log('user 保存失败:', err);
+// });
+
+// Retrieve
+// 找出 users collections 中的所有数据
+User.find().then(docs => {
+    console.log('[Promise]users all:');
+    console.log(docs);
+});
+
+User.find((err, docs) => {
+    console.log('[callback]users all:');
+    console.log(docs);
+});
+
+// 找出 users collections 中 name 为 aaa 的所有数据
+User.find({
     name: 'aaa'
+}, (err, docs) => {
+    console.log('users of name is "aaa":');
+    console.log(docs);
 });
-user.save().then(() => {
-    console.log('user 保存成功');
-}, res => {
-    console.log('user 保存失败:', err);
-});
+
+// Update
+// 两种写法之一：Promise
+// User.update({
+//     name: 'bbb'
+// }, {
+//     name: 'ccc'
+// }).then(raw => {
+//     console.log('数据更新成功:', raw);
+// });
+// 两种写法之二：Callback
+User.update({
+    name: 'bbb'
+}, {
+    name: 'ccc'
+}).exec();
+
+// Remove
+User.remove({
+    name: 'aaa'
+}).exec();
