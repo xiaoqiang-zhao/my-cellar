abfcfbf3f541f340af65278ef08edbaac6200f21
longze <longze_xq@163.com>
2018-02-05 10:35:42 +0800

    添加demo代码高亮功能: remark

--- web/articles/markdown-remark/demo/demo-in-browser/src/pages/home/index.vue
+++ web/articles/markdown-remark/demo/demo-in-browser/src/pages/home/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="hello">
     <textarea name="md" cols="30" rows="10" v-model="mdText"></textarea>
-    <div v-html="mdHTML"></div>
+    <div class="markdown-body" v-html="mdHTML"></div>
   </div>
 </template>
 
 <script>
-import remark from 'remark'
-import styleGuide from 'remark-preset-lint-markdown-style-guide'
+import unified from 'unified'
+import markdown from 'remark-parse'
+// import remark from 'remark'
+// import styleGuide from 'remark-preset-lint-markdown-style-guide'
 import html from 'remark-html'
 import report from 'vfile-reporter'
+import highlight from 'remark-highlight.js'
+import 'github-markdown-css'
 
 export default {
   mounted () {
@@ -26,15 +30,27 @@ export default {
 
 >说明文字。
 
-## 二级标题`,
+## 二级标题
+
+~~~css
+h1 {
+  color: red;
+}
+~~~
+
+~~~jsvascript
+let a = '';
+console.log(a)
+`,
       mdHTML: ''
     }
   },
   methods: {
     md () {
       let me = this
-      remark()
-        .use(styleGuide)
+      unified()
+        .use(markdown)
+        .use(highlight)
         .use(html)
         .process(this.mdText, (err, file) => {
           console.error(report(err || file))
--- web/articles/markdown-remark/main.md
+++ web/articles/markdown-remark/main.md
@@ -14,3 +14,6 @@
 https://github.com/remarkjs/remark
 
 公司的高T原话：“js 玩 markdown 建议 remark，其他的都是早晚后悔 ”。
+
+[highlight](https://highlightjs.org/usage/)
+
