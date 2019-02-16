aee55ed24749f023486bcadafee83bab3f3f5180
xiaoqiang-zhao <longze_xq@163.com>
2019-02-16 22:33:52 +0800

    现价Demo：流式读写-生成大文件

--- web/articles/node-base/demo/fs/createReadStream-createWriteStream-big.mjs
+++ web/articles/node-base/demo/fs/createReadStream-createWriteStream-big.mjs
@@ -1,5 +1,5 @@
 /**
- * @file 流式读写 Demo
+ * @file 流式读写 Demo，通过重复拷贝小文件内容生成大文件
  */
 
 import fs from 'fs';
@@ -8,7 +8,7 @@ import fs from 'fs';
 const start = new Date().getTime();
 
 // 创建一个可读流（生产者）
-const rs = fs.createReadStream('./rw-log2', {
+const rs = fs.createReadStream('./rw-log', {
   encoding: 'utf-8'
 });
 
@@ -20,11 +20,11 @@ printMemoryUsage();
 
 rs.on('data', chunk => {
   // 制造大文件，1G 重复 14 次
-  // for (var i = 0; i < 14; i++) {
+  for (var i = 0; i < 14; i++) {
     ws.write(chunk, () => {
       // console.log('一次写入完成');
     });
-  // }
+  }
 });
 
 rs.on('end', () => {
