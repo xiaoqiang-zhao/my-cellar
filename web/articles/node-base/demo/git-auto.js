e2c0350c8cc5b2e2d34a1f6a813e8d2dce75e4b5
xiaoqiang-zhao <longze_xq@163.com>
2019-10-04 23:54:00 +0800

    提交编译内容

--- web/articles/node-base/demo/git-auto.js
+++ web/articles/node-base/demo/git-auto.js
@@ -3,6 +3,12 @@
  * @author xiaoqiang-zhao
  */
 
+/** == == == 
+ * 注意: 执行的时候如果锁屏，执行的时间可能会不准确，后台会一小时左右执行一次任务。
+ *      所以此程序的策略是，判断执行时的时间是否超过了预设，如果超过了就执行。
+ *      为了保证不错过后台定期任务巡视，采用每秒轮训一次的策略进行。
+ * == == == **/
+
 const spawn = require('child_process').spawn;
 const options = {
     // 指定执行路径
@@ -16,10 +22,10 @@ const taskList = [
     {
         // 提交文件
         files: [
-            '.'
+            'web/articles/android-native/main.md'
         ],
-        hours: 0.1,
-        message: '添加编译内容'
+        hours: 5,
+        message: '添加内容: android-native / activity'
     }
 ];
 
@@ -93,10 +99,32 @@ function delayTask(taskConfig, task) {
         // 时间，小时转毫秒
         const time = 1000 * 60 * 60 * taskConfig.hours;
         console.log(`任务开始倒计时执行: ${taskConfig.message}, ${getCurrentDateStr()}`);
-        setTimeout(() => {
+        // setTimeout(() => {
+        //     task(resolve, reject);
+        // }, time);
+        const currentDate = new Date();
+        const entDate = new Date(currentDate.getTime() + time);
+        superSetTimeout(() => {
             task(resolve, reject);
-        }, time);
+        }, entDate);
     });
+}
+
+/**
+ * 
+ * @param {function} cb 回调函数
+ * @param {Object} endDate 结束时间
+ */
+function superSetTimeout(cb, endDate) {
+    console.log(`任务扫描记录: ${getCurrentDateStr()}`);
+    if (new Date().getTime() > endDate.getTime()) {
+        cb();
+    }
+    else {
+        setTimeout(() => {
+            superSetTimeout(cb, endDate);
+        }, 1000);
+    }
 }
 
 /**
@@ -124,3 +152,11 @@ taskList.forEach((item, index) => {
 });
 
 console.log('-----------------');
+
+function t() {
+    console.log(getCurrentDateStr());
+    setTimeout(() => {
+        t();
+    }, 1000);
+}
+t();
